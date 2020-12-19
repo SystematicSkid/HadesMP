55977c39b9e58b06b8757454013e2e4961fcfa83
SystematicSkid <superseb123abc@gmail.com>
2020-12-19 16:27:04 -0600

    Add lua state pointer.

--- Hades-LuaLoader/Engine/Classes/scriptmanager.h
+++ Hades-LuaLoader/Engine/Classes/scriptmanager.h
@@ -5,9 +5,13 @@ namespace engine
 	class ScriptManager
 	{
 	public:
+		static DWORD64*& lua_state;
+	public:
 		static bool LoadFile(const char* name)
 		{
 			return static_cast<BOOLEAN(__fastcall*)(const char*)>((PVOID)engine::addresses::lua::functions::loadfile)(name);
 		}
 	};
+
+	DWORD64*& engine::ScriptManager::lua_state = *(DWORD64**)(engine::addresses::lua::state);
 }
